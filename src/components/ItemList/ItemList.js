024e5c6a52f0242c3197c11f76bc252e18892370
Juan Cruz Garcilazo <garcilazo.juancruz@hotmail.com>
2022-03-11 00:08:32 -0300

    MaterialUI quitado

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,12 +4,15 @@ import Item from '../ItemList/Item'
 function ItemList ({items}) {
     return (
         items.map(p =>
-            <Item key = {p.id}
-            title = {p.title}
-            pictureUrl = {p.pictureUrl}
-            price = {p.price}
-            stock = {p.stock}
-            />
+            <div>
+                <Item key = {p.id}
+                title = {p.title}
+                pictureUrl = {p.pictureUrl}
+                price = {p.price}
+                stock = {p.stock}
+                />
+            </div>
+            
         )
     )
 }
